4f5c50218ee0b921f0d05ce805921cc5c4b9967c
Tristan Struthers <trstruth@microsoft.com>
2021-03-10 10:04:33 -0800

    Begin to add support for various options: num-iterations, scale, output, target path

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,54 @@
 extern crate image;
 extern crate rand;
 
-use std::error::Error;
 use rand::Rng;
+use std::env;
+use std::error::Error;
 
 mod canvas;
 mod emoji;
 
-const CANVAS_WIDTH: u32 = 1920;
-const CANVAS_HEIGHT: u32 = 1080;
-const NUM_ITERATIONS: u32 = 200000;
-// const NUM_EMOJIS: u32 = 2363;
+/*
+ * arg: path to image
+ * -n --num-iterations (20000): number of times to place an emoji
+ * -o --output (canvas.png): output file path
+ * -s --scale (1): resolution ratio of output:target images
+*/
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let num_iterations = 500000;
+    let output = "canvas.png";
+    let scale = 3;
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Please provide a path to the target image");
+    }
+    let target_path = &args[1];
+    let target = image::open(target_path).unwrap();
+    let target_rgba = target.to_rgba();
+    let (width, height) = target_rgba.dimensions();
+
     println!("Generating new EmojiManager");
     let mut em = emoji::EmojiManager::new("emojis")?;
     println!("Generating new CanvasManager");
-    let mut cm = canvas::CanvasManager::new("canvas.png", CANVAS_WIDTH, CANVAS_HEIGHT)?;
+    let mut cm = canvas::CanvasManager::new(output, width * scale, height * scale)?;
 
     // let target = image::open("cyberpunk.png").unwrap();
     // let target = image::open("basquiat.jpg").unwrap();
-    let target = image::open("bladerunner.png").unwrap();
-    let target_rgba = target.to_rgba();
+    // let target = image::open("bladerunner.png").unwrap();
 
     println!("Placing emojis");
 
-    /*
-    for (x, y, p) in target.to_rgba().enumerate_pixels() {
-        let nearest_emoji_id = em.get_nearest_emoji_id(*p);
-        let emoji = em.get_emoji(nearest_emoji_id).unwrap();
-        cm.place_emoji(emoji, x, y);
-    }
-    */
-
     let mut rng = rand::thread_rng();
-    for  _ in 0..NUM_ITERATIONS {
-        let rand_x = rng.gen_range(0, CANVAS_WIDTH);
-        let rand_y = rng.gen_range(0, CANVAS_HEIGHT);
+    for _ in 0..num_iterations {
+        let rand_x = rng.gen_range(0, width);
+        let rand_y = rng.gen_range(0, height);
         let target_p = target_rgba.get_pixel(rand_x, rand_y);
 
         let nearest_emoji_id = em.get_nearest_emoji_id(*target_p);
         let emoji = em.get_emoji(nearest_emoji_id).unwrap();
-        cm.place_emoji(emoji, rand_x, rand_y);
+        cm.place_emoji(emoji, rand_x * scale, rand_y * scale);
     }
 
     println!("Saving canvas");
